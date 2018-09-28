4883257f0f87d7ba201d529d177fce80fecbd905
Kristian Jerpetjøn <kristian.jerpetjoen@gmail.com>
2018-09-28 23:23:45 +0200

    Created cmake sub for libos.a

--- src/drivers/vmxnet3.cpp
+++ src/drivers/vmxnet3.cpp
@@ -1,4 +1,4 @@
-﻿// This file is a part of the IncludeOS unikernel - www.includeos.org
+// This file is a part of the IncludeOS unikernel - www.includeos.org
 //
 // Copyright 2015 Oslo and Akershus University College of Applied Sciences
 // and Alfred Bratterud
