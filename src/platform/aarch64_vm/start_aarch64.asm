2b5424d41ab8c0ec31dcc587bdf97994f0f9f0e8
Kristian Jerpetjoen <kristian.jerpetjoen@gmail.com>
2019-05-11 00:17:16 +0200

    rpi3: some RPI code required

--- src/platform/aarch64_vm/start_aarch64.asm
+++ src/platform/aarch64_vm/start_aarch64.asm
@@ -1,23 +1,16 @@
-.globl __boot_magic
-
-.data
-.align 8
-__boot_magic:
-  .dword 0
+.extern __boot_magic
 
 .text
 .align 8
 .globl _start
 _start:
+  // in case someone one day provides us with a cookie
+  ldr x8 , =__boot_magic
+  str x0, [x8]
   b reset
 
 .globl reset
 reset:
-  //why ?
-  //mrs x2, S3_1_C15_C3_0 // Read CBAR_EL1 into X2
 
- // in case someone one day provides us with a cookie
-  ldr x8 , __boot_magic
-  str x0, [x8]
 
   b __arch_start
